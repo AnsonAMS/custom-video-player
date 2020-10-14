0e608b826dc1d9bdb5941e77fd70b67191437ce0
AnsonAMS <anson_tohata@hotmail.com>
2020-10-14 16:56:41 +0200

    Caching selectors into variables

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
-
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progressing = document.getElementById('progressing');
+const timestap = document.getElementById('timestamp');
