df274dc1f800e75d495b997ad2f57d0cb6b24f39
AnsonAMS <anson_tohata@hotmail.com>
2020-10-14 17:35:59 +0200

    Setting up play, pause and stop functionality

--- script.js
+++ script.js
@@ -4,6 +4,39 @@ const stop = document.getElementById('stop');
 const progressing = document.getElementById('progressing');
 const timestap = document.getElementById('timestamp');
 
+// Play & pause video
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
+
+// Update play/pause icon
+function updatePlayIcon() {
+    if (video.paused) {
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
+
+// Update progress & timestamp
+function updateProgress() {
+    return true;
+}
+
+// Set video time to progress
+function setVideoProgress() {
+    return true;
+}
+
+// Stop video 
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
 
 // Event Listeners
 video.addEventListener('click', toggleVideoStatus);
