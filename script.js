a5409c3eb54ec35652940b4a3c1a56bfcaa0125d
AnsonAMS <anson_tohata@hotmail.com>
2020-10-14 17:48:54 +0200

    Adding video progress bar and timestamp

--- script.js
+++ script.js
@@ -1,44 +1,58 @@
 const video = document.getElementById('video');
 const play = document.getElementById('play');
 const stop = document.getElementById('stop');
-const progressing = document.getElementById('progressing');
-const timestap = document.getElementById('timestamp');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
 
 // Play & pause video
 function toggleVideoStatus() {
-    if(video.paused) {
-        video.play();
-    } else {
-        video.pause();
-    }
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
 }
 
-// Update play/pause icon
+// update play/pause icon
 function updatePlayIcon() {
-    if (video.paused) {
-        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
-    } else {
-        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
-    }
+  if (video.paused) {
+    play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+  } else {
+    play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+  }
 }
 
 // Update progress & timestamp
 function updateProgress() {
-    return true;
+  progress.value = (video.currentTime / video.duration) * 100;
+
+  // Get minutes
+  let mins = Math.floor(video.currentTime / 60);
+  if (mins < 10) {
+    mins = '0' + String(mins);
+  }
+
+  // Get seconds
+  let secs = Math.floor(video.currentTime % 60);
+  if (secs < 10) {
+    secs = '0' + String(secs);
+  }
+
+  timestamp.innerHTML = `${mins}:${secs}`;
 }
 
 // Set video time to progress
 function setVideoProgress() {
-    return true;
+  video.currentTime = (+progress.value * video.duration) / 100;
 }
 
-// Stop video 
+// Stop video
 function stopVideo() {
-    video.currentTime = 0;
-    video.pause();
+  video.currentTime = 0;
+  video.pause();
 }
 
-// Event Listeners
+// Event listeners
 video.addEventListener('click', toggleVideoStatus);
 video.addEventListener('pause', updatePlayIcon);
 video.addEventListener('play', updatePlayIcon);
@@ -48,5 +62,4 @@ play.addEventListener('click', toggleVideoStatus);
 
 stop.addEventListener('click', stopVideo);
 
-progress.addEventListener('click', setVideoProgress);
-
+progress.addEventListener('change', setVideoProgress);
